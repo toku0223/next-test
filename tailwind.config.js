e22ac610ba81e51b9a0d81693832fcf446caac0f
toku0223 <kei.nakanotoku@gmail.com>
2023-03-11 10:26:48 +0900

    main

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'slide-in-right':
+          'slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both',
+      },
+      keyframes: {
+        'slide-in-right': {
+          '0%': {
+            transform: 'translateX(1000px)',
+            opacity: '0',
+          },
+          to: {
+            transform: 'translateX(0)',
+            opacity: '1',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
